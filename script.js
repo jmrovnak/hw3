9221e13c062c71f9883efabc08f7769f3d804ad6
Justin Rovnak <jmrovnak@gmail.com>
2020-12-30 12:33:44 -0600

    added landing prompt and password length alerts

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+var passLength = prompt("How many characters would you like your password to contain?")
+
+if (passLength < 8) {
+    alert("Fatal Error. Choose more characters or evacuate immediately.")
+}
+
+if (passLength > 128) {
+    alert("Silly rabbit. Too many characters are for losers.")
+}
+
+
+
+
+
+var generateBtn = document.querySelector("#generate")
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  
   passwordText.value = password;
 
 }
