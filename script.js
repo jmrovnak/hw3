4f93c84f2a9418da09f23b00ffad40ea2a4e2080
Justin Rovnak <jmrovnak@gmail.com>
2021-01-01 16:34:44 -0600

    started over :(

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
 // Assignment Code
 
-var passLength = prompt("How many characters would you like your password to contain?")
-var lowerCase = "Would you like to include lowercase characters?"
-var upperCase = "Would you like to include uppercase characters?"
-var numeric = "Would you like to include numeric characters?"
 
-
-if (passLength < 8) {
-    alert("Fatal Error. Choose more characters or evacuate immediately.")
-}
-
-if (passLength > 128) {
-    alert("Silly rabbit. Too many characters are for losers.")
-}
-
-if (passLength > 7 && passLength < 129) {
-    alert("Excellent choice!")
-}
-
-
-
-
-
-
-
+12
 
 var generateBtn = document.querySelector("#generate")
 
