0b3867cca0adbcaa3bfc116d68bb2790e00a2d3a
Justin Rovnak <jmrovnak@gmail.com>
2021-01-01 18:02:45 -0600

    declared variable for password length, set alert for improper length

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // Assignment Code
+var passLength = prompt("Please choose the number of characters you'd like your password to contain.")
 
+if (passLength > 128 || passLength < 8) {
+    alert("Your password must contain between 8 and 128 characters")
+}
 
-12
-
-var generateBtn = document.querySelector("#generate")
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  
   passwordText.value = password;
 
 }
