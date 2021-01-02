5ec9e735fefdbba14a61713b594ef078727dd6b7
Justin Rovnak <jmrovnak@gmail.com>
2021-01-01 18:37:45 -0600

    added comments, alert if at least one character type not selected

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Assignment Code
 var passLength = prompt("Please choose the number of characters you'd like your password to contain.") 
 
+//This will alert the user if password length is unacceptable
 if (passLength > 128 || passLength < 8) {
     alert("Your password must contain between 8 and 128 characters")
+    
 }
 
+//This will prompt the user to select preferred special character types
 if (passLength <= 128 && passLength >= 8) {
     var lowerCase = confirm("Would you like your password to contain lowercase letters?")
     var upperCase = confirm("Would you like your password to contain uppercase letters?")
@@ -12,6 +15,13 @@ if (passLength <= 128 && passLength >= 8) {
     var special = confirm("Would you like your password to contain special characters?")
 
 }
+
+// This will alert the user to select at least one character type
+if (lowerCase !== true && upperCase !== true && wholeNumber !== true && special !== true) {
+    alert("Please choose at least one character type.")
+}
+
+
 
 
 
