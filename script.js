575613a87f4c8c7276793b19bba6540dd11edf95
Justin Rovnak <jmrovnak@gmail.com>
2021-01-01 18:18:52 -0600

    declared variables for character types

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // Assignment Code
-var passLength = prompt("Please choose the number of characters you'd like your password to contain.")
+var passLength = prompt("Please choose the number of characters you'd like your password to contain.") 
 
 if (passLength > 128 || passLength < 8) {
     alert("Your password must contain between 8 and 128 characters")
 }
+
+if (passLength <= 128 && passLength >= 8) {
+    var lowerCase = prompt("Would you like your password to contain lowercase letters?")
+    var upperCase = prompt("Would you like your password to contain uppercase letters?")
+    var wholeNumber = prompt("Would you like your password to contain numeric characters?")
+    var special = prompt("Would you like your password to contain special characters?")
+
+
+
+
 
 var generateBtn = document.querySelector("#generate");
 
